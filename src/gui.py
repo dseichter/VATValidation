2bb3afa3a6162448e21220ba924f6c0cefce88aa
Daniel Seichter <daniel.seichter@dseichter.de>
2025-08-10 16:22:00 +0200

    Fix typo: XML -> XSLX

--- src/gui.py
+++ src/gui.py
@@ -192,7 +192,7 @@ class MainFrame ( wx.Frame ):
         fgSizer3.Add( self.staticText_Inputfile, 0, wx.ALL, 5 )
 
         self.filepickerInput = wx.FilePickerCtrl( self.panelBatch, wx.ID_ANY, wx.EmptyString, _(u"Select a file"), _(u"*.*"), wx.DefaultPosition, wx.DefaultSize, wx.FLP_DEFAULT_STYLE )
-        self.filepickerInput.SetToolTip( _(u"Select your input file (JSON, XML, CSV) for processing.") )
+        self.filepickerInput.SetToolTip( _(u"Select your input file (JSON, XSLX, CSV) for processing.") )
 
         fgSizer3.Add( self.filepickerInput, 1, wx.ALL|wx.EXPAND, 5 )
 
