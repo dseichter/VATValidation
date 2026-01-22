162f937e691b13d2a1be27a3b0c93da355b82256
Daniel Seichter <daniel.seichter@dseichter.de>
2026-01-22 21:23:24 +0100

    New release

--- src/helper.py
+++ src/helper.py
@@ -20,7 +20,7 @@ from packaging import version
 
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
-VERSION = "v2026-01-15"
+VERSION = "v2026-01-22"
 UPDATEURL = 'https://api.github.com/repos/dseichter/VATValidation/releases/latest'
 RELEASES = 'https://github.com/dseichter/VATValidation/releases'
 NAME = 'VATValidation'
